d7a52faabe43c351cfc9e3072a8f9c6183c2e1dc
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-13 21:09:51 +0000

    components setup

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-post-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+})
+export class MenuComponent {}
