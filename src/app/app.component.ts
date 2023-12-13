05d0000811193d66b6c98fe8202a02cd597ceba0
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-13 22:01:37 +0000

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, MenuComponent, FooterComponent],
+  imports: [RouterModule, MenuComponent],
   selector: 'angular-post-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FooterComponent } from 'src/app/shared/footer/footer.component';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
 
 @Component({
   selector: 'angular-post-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
