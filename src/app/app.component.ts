178d87572513f166ed3c772d3f4913daac6a399e
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-13 22:19:50 +0000

    tailwind updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
 import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule, MenuComponent],
-  selector: 'angular-post-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    standalone: true,
+    selector: 'angular-post-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterModule, MenuComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-post';
