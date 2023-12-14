e0278ab5486bf8642c69bfe6102c5f658da686bf
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-14 15:20:33 +0000

    refactor

---
+++ src/app/shared/horizontal-line/horizontal-line.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-horizontal-line',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './horizontal-line.component.html',
+  styleUrl: './horizontal-line.component.scss',
+})
+export class HorizontalLineComponent {}
--- src/app/store/videos/videos.reducer.ts
+++ src/app/store/videos/videos.reducer.ts
@@ -5,6 +5,36 @@ import { getVideos } from './videos.actions';
 export const initialState = [{
   id: 123,
   link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+}, {
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
+},{
+  id: 123,
+  link: "https://www.youtube.com/embed/v4HkkjXmEMM?si=-EhqAthFjE4-AAhF"
 }];
 
 export const videosReducer = createReducer(
