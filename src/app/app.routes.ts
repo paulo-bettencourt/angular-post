3679333188df83e278d108fff5d0375124425421
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-12-16 12:54:49 +0000

    router module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Route } from '@angular/router';
 
+import { AuthComponent } from './admin/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const appRoutes: Route[] = [{
   path: '',
   loadComponent: () => import('./components/home/home.component').then(() => HomeComponent)
+},
+{
+  path: 'login',
+  loadComponent: () => import('./admin/auth/auth.component').then(() => AuthComponent)
 }];
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HorizontalLineComponent } from '../horizontal-line/horizontal-line.component';
 
 @Component({
   selector: 'angular-post-header',
   standalone: true,
-  imports: [CommonModule, HorizontalLineComponent],
+  imports: [CommonModule, HorizontalLineComponent, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
