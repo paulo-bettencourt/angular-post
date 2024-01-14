6a74abbddf30752dcb70d47e6bc6df468aeda49a
paulolemos-overflow <paulo.lemos@bringglobal.com>
2024-01-14 12:48:28 -0100

    oauth setup

--- environments/environment.ts
+++ environments/environment.ts
@@ -2,12 +2,14 @@ export const environment = {
   production: false,
   API_POSTS: 'https://angular-post-java.onrender.com/api/posts',
   API_AUTH_REGISTER: 'https://angular-post-java.onrender.com/authentication/register',
-  API_AUTH_LOGIN: 'https://angular-post-java.onrender.com/authentication/login'
+  API_AUTH_LOGIN: 'https://angular-post-java.onrender.com/authentication/login',
+  firebaseConfig: {
+    apiKey: "AIzaSyDTjgCp2mQfB9QpF2zmjdkk4dUclBHlgOg",
+    authDomain: "angular-post-1a202.firebaseapp.com",
+    projectId: "angular-post-1a202",
+    storageBucket: "angular-post-1a202.appspot.com",
+    messagingSenderId: "98752778733",
+    appId: "1:98752778733:web:b32fe4efa83a7fff1c17e4"
+  }
 };
 
-/* export const environment2 = {
-  production: false,
-  API_POSTS: 'http://localhost:8080/api/posts',
-  API_AUTH_REGISTER: 'http://localhost:8080/authentication/register',
-  API_AUTH_LOGIN: 'http://localhost:8080/authentication/login'
-}; */
--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { environment } from 'environments/environment';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { ApiService } from 'src/app/services/api.service';
+
+initializeApp(environment.firebaseConfig);
 
 @Component({
   selector: 'angular-post-auth',
@@ -14,6 +19,7 @@ import { ApiService } from 'src/app/services/api.service';
 export default class LoginComponent {
   form = inject(FormBuilder);
   apiService = inject(ApiService);
+  router = inject(Router);
   activatedRoute = inject(ActivatedRoute);
   loginForm = this.form.nonNullable.group({
     username: ['', Validators.required],
@@ -21,18 +27,45 @@ export default class LoginComponent {
   });
   paramsRoute = '';
   isLogged = false;
-  view = 'login'
+  view = 'login';
+  auth = getAuth();
+  provider = new GoogleAuthProvider();
 
+  // submitForm() {
+  //   if (this.loginForm.valid) {
+  //     const auth = this.loginForm.value;
+  //     this.apiService.login(auth).subscribe((data) => {
+  //       this.isLogged = true;
+  //       this.view = data.auth;
+  //     });
+  //   } else {
+  //     // Form is invalid, handle accordingly
+  //   }
+  // }
 
   submitForm() {
-    if (this.loginForm.valid) {
-      const auth = this.loginForm.value;
-      this.apiService.login(auth).subscribe((data) => {
-        this.isLogged = true;
-        this.view = data.auth;
+    signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log('TOKEN: ', token);
+        console.log('USER: ', user);
+        this.router.navigate(['/about'])
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
       });
-    } else {
-      // Form is invalid, handle accordingly
-    }
   }
 }
