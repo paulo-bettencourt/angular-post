05a0de255623e75fdebd9f803178dea04d1800f1
Paulo Bettencourt <pauloblemos@gmail.com>
2024-01-15 13:21:35 -0100

    login email works

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { environment } from 'environments/environment';
 import { initializeApp } from 'firebase/app';
 import {
+  browserSessionPersistence,
+  connectAuthEmulator,
   FacebookAuthProvider,
   getAuth,
   GoogleAuthProvider,
+  inMemoryPersistence,
+  setPersistence,
+  signInWithEmailAndPassword,
   signInWithPopup,
+  signInWithRedirect,
 } from 'firebase/auth';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -21,13 +27,13 @@ initializeApp(environment.firebaseConfig);
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export default class LoginComponent {
+export default class LoginComponent implements OnInit {
   form = inject(FormBuilder);
   apiService = inject(ApiService);
   router = inject(Router);
   activatedRoute = inject(ActivatedRoute);
   loginForm = this.form.nonNullable.group({
-    username: ['', Validators.required],
+    email: ['', Validators.required],
     password: ['', Validators.required],
   });
   paramsRoute = '';
@@ -36,7 +42,28 @@ export default class LoginComponent {
   auth = getAuth();
   provider = new GoogleAuthProvider();
 
-  signInWith(provider: string) {
+  ngOnInit(): void {
+    connectAuthEmulator(this.auth, 'http://127.0.0.1:9099');
+  }
+
+  submitForm() {
+    const email = this.loginForm.value.email;
+    const password = this.loginForm.value.password;
+
+    if (email && password) {
+      setPersistence(this.auth, browserSessionPersistence)
+        .then(() => {
+          return signInWithEmailAndPassword(this.auth, email, password);
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    }
+  }
+
+  signInGoogle() {
     signInWithPopup(this.auth, this.provider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
