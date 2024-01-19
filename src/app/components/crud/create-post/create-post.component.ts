8af54157ac24b35db652904f2ae41240ce30dc18
Paulo Bettencourt <pauloblemos@gmail.com>
2024-01-19 18:19:41 -0100

    form

--- src/app/components/crud/create-post/create-post.component.ts
+++ src/app/components/crud/create-post/create-post.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'angular-post-create-post',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-post.component.html',
   styles: ``,
 })
-export default class CreatePostComponent {}
+export default class CreatePostComponent {
+  fb = inject(FormBuilder);
+  createPostForm = this.fb.group({
+    author: [''],
+    title: ['', Validators.required],
+    text: ['', Validators.required],
+    youtubeId: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log('on submit');
+  }
+}
