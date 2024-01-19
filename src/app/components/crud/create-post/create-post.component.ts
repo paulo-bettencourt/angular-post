b176b2df54c7f0166f75eb1628a40bd497b9550f
Paulo Bettencourt <pauloblemos@gmail.com>
2024-01-19 19:50:23 -0100

    ngTemplateOutlet

--- src/app/components/crud/create-post/create-post.component.ts
+++ src/app/components/crud/create-post/create-post.component.ts
@@ -1,5 +1,9 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Injector, inject } from '@angular/core';
+import {
+  CommonModule,
+  NgComponentOutlet,
+  NgTemplateOutlet,
+} from '@angular/common';
 import { Validators } from '@angular/forms';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { DatabaseService } from 'src/app/services/database/database.service';
@@ -7,22 +11,25 @@ import { DatabaseService } from 'src/app/services/database/database.service';
 @Component({
   selector: 'angular-post-create-post',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgTemplateOutlet],
   templateUrl: './create-post.component.html',
   styles: ``,
 })
 export default class CreatePostComponent {
   dbService = inject(DatabaseService);
   fb = inject(FormBuilder);
+  formInjector!: Injector;
   createPostForm = this.fb.nonNullable.group({
     author: ['1234', Validators.required],
     title: ['', [Validators.required, Validators.minLength(5)]],
     description: ['', [Validators.required, Validators.minLength(5)]],
     youtubeId: [''],
   });
+  isSubmitted = false;
 
   onSubmit() {
     console.log('on submit: ', typeof this.createPostForm.getRawValue());
     this.dbService.writePost(this.createPostForm.getRawValue());
+    this.isSubmitted = true;
   }
 }
